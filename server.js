33026372ac87e0b1e59d185b2ed3baddacc54798
xLang1234 <leongohxx@gmail.com>
2023-11-24 22:26:28 +0800

    Resolve linkage

--- server.js
+++ server.js
@@ -9,8 +9,40 @@ const port = process.env.PORT || 3000;
 connectDB();
 app.use(express.json());
 
-app.use("/api", routes);
+app.use("/", routes);
 
 app.listen(port, () => {
   logger.info(`Server running on http://localhost:${port}`);
 });
+
+function listAllRoutes() {
+  const routes = [];
+
+  app._router.stack.forEach((middleware) => {
+    if (middleware.route) {
+      // routes registered directly on the app
+      routes.push({
+        path: middleware.route.path,
+        method: Object.keys(middleware.route.methods)[0].toUpperCase(),
+      });
+    } else if (middleware.name === "router") {
+      // router middleware
+      middleware.handle.stack.forEach((handler) => {
+        if (handler.route) {
+          const routePath = middleware.regexp.exec(handler.route.path)
+            ? middleware.regexp.exec(handler.route.path)[0].replace("\\", "")
+            : "";
+          const completePath = routePath + handler.route.path;
+          routes.push({
+            path: completePath,
+            method: Object.keys(handler.route.methods)[0].toUpperCase(),
+          });
+        }
+      });
+    }
+  });
+
+  return routes;
+}
+
+console.log(listAllRoutes());
